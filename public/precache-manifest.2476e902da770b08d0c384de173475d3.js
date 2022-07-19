608c0273751b1e654606fbb66a009363e6923310
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-07-19 15:52:16 -0500

    Frontend added in Public folder

---
+++ public/precache-manifest.2476e902da770b08d0c384de173475d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b3a53419baff06ebee9245748c2746f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e6248b1290ea2319afd",
+    "url": "/static/css/2.c7de1cb5.chunk.css"
+  },
+  {
+    "revision": "1082efb090b469917e42",
+    "url": "/static/css/main.9443ed09.chunk.css"
+  },
+  {
+    "revision": "2e6248b1290ea2319afd",
+    "url": "/static/js/2.061a37a2.chunk.js"
+  },
+  {
+    "revision": "afb90854ca170d941f072bc07eb8c0b6",
+    "url": "/static/js/2.061a37a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1082efb090b469917e42",
+    "url": "/static/js/main.81e476e5.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.81e476e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
